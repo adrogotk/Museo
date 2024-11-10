b878510964f5d042c0d201a6e6e7157c37aaf3cf
adrogotk <60137058+adrogotk@users.noreply.github.com>
2024-11-10 16:13:52 +0100

    Update App.java

--- src/es/etg/psp/museo/App.java
+++ src/es/etg/psp/museo/App.java
@@ -1,1 +1,10 @@
+package es.etg.psp.museo;
+
+public class App {
+
+    public static void main(String[] args) {
+			Museo m=new Museo;
+			int totalPersonas=m.calcularAforo();
+      System.out.println(totalPersonas);
+	}
 
